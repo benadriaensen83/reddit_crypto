32eb6f9b615145c121e2ba8cbfa1d0421d14187c
loc-asb1 <Kolossel1>
2020-02-11 15:43:47 +0100

    e

--- data_collect.py
+++ data_collect.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 from tabulate import tabulate
+import datetime
 
 class DataCollect:
 
@@ -8,23 +9,27 @@ class DataCollect:
 
         url = "https://api.pushshift.io/reddit/comment/search"
 
-        querystring = {"sort":"%27asc%27","q":"%27bitcoin%27","size":"20"}
+        querystring = {"sort": "%27asc%27", "q": "%27bitcoin%27", "size": "34"}
 
         headers = {
             'User-Agent': "PostmanRuntime/7.13.0",
             'Accept': "*/*",
             'Cache-Control': "no-cache",
-            'Postman-Token': "7b7818c5-f2bd-4962-950d-31c6ebc5ac94,661f03fd-9fb7-4734-a8b2-3b8ff3a1d19f",
+            'Postman-Token': "d5979873-a5b6-4e5f-b40f-32458e7082c9,4fcaadea-15a7-49d8-8b05-891336fa493b",
             'Host': "api.pushshift.io",
             'cookie': "__cfduid=df6ffa9e102960abbcbe896ecfd332d071581425174",
             'accept-encoding': "gzip, deflate",
             'Connection': "keep-alive",
             'cache-control': "no-cache"
-            }
+        }
 
         response = requests.request("GET", url, headers=headers, params=querystring)
 
+        print(response.text)
+
         data = response.json()['data']
 
+        print(len(data))
+        exit()
         return data
 
--- run.py
+++ run.py
@@ -1,11 +1,17 @@
 import data_collect
 import pandas as pd
 from tabulate import tabulate
-
+from datetime import datetime
 
 run = data_collect.DataCollect()
+
 data = run.data_from_api()
-print(len(data))
+
+# adjust UNIX timestamp from API to readable datetime
+
+for i in range(len(data)):
+    data[i]['created_adj'] = datetime.fromtimestamp(data[i]['created_utc']).strftime('%Y-%m-%d %H:%M:%S')
+
 data = pd.DataFrame(data)
 print(tabulate(data, headers = 'keys', tablefmt= 'fancy_grid'))
 pd.DataFrame.to_csv(data, 'reddit_comments.csv')
